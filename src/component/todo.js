4c9052656c68ffe56c25056bd2895c24adb8c545
Vikas1211 <vikas1211singh@gmial.com>
2021-11-22 18:50:19 +0530

    first commit

---
+++ src/component/todo.js
@@ -0,0 +1,68 @@
+import React, {useState} from 'react'
+import todo from "../images/todo.png";
+
+const Todo = () => {
+
+    const [inputdata, setInputData] = useState('');
+    const [item, setItems] = useState([]);
+    
+    const addItem = () => {
+        if(inputdata){
+            setItems([...item,inputdata]); 
+            setInputData('');
+        }
+    }
+
+    const delItem = (id) => {
+        const updatedItem = item.filter((elem, ind) =>{
+            return ind != id;
+        });
+
+        setItems(updatedItem);
+    }
+
+    const delAll =() => {
+        setItems([]);
+    }
+
+    return (
+        <>
+            <div className="main-div App">
+                <div className="child-div">
+                    <figure>
+                        <img className="App-logo" src={todo} alt="todologo"/>
+                        <figcaption>ADD YOUR LIST HERE </figcaption>
+                    </figure>
+
+                    <div className="addItems">
+                        <input type="text" placeholder="Add Items..."
+                        value={inputdata}
+                        onChange={(e) => setInputData(e.target.value)}/>
+                        <i className="fa fa-plus add-btn " title="Add Item" onClick={addItem}></i>
+                    </div>
+
+                    <div className="showItem">
+
+                        {
+                            item.map((elem,ind) => {
+                                return(
+                                    <div className="EItem" key={ind}>
+                                    <h5>{elem}<i className="far fa-trash-alt add-btn " title="Delete Item" onClick={() => delItem(ind)}></i> </h5>
+                                    
+                                </div>
+                                )
+                            })
+                        }
+                        
+                    </div>
+                    <div className="showItem">
+                        <button className="btn btn-primary" onClick={delAll}>Remove All</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Todo
+
